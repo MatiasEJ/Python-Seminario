c2bd4d730380cd590f5e8f044028287cdd35d097
MatiasEJ <don.juncos@gmail.com>
2021-10-15 07:41:47 -0300

    ej_7

--- ej_7_2.py
+++ ej_7_2.py
@@ -1,19 +1,47 @@
 import UTIL
 
+def maximo(lista):
+    i = 0
+    mayor = lista[0]
+    while i < len(lista):
+        if (int(lista[i])>mayor):
+            mayor=lista[i]
+        i+=1
+    return mayor
+
+def menor(lista):
+    i = 0
+    menor = lista[0] 
+    while i < len(lista):
+        if (int(lista[i])<menor):
+            menor=lista[i]
+        i+=1
+    return menor
+
+def promedio(lista):
+    total = 0
+    for num in lista:
+        total += num
+    return total/len(lista) if len(lista)>0 else 0
+
+def lineas_totales(archivo):
+    fileref = open(archivo,'r')
+    cant_lineas = len(fileref.readlines())
+    fileref.close()
+    return cant_lineas
+
 def informe(archivo)-> list:
     fileref = open(archivo,'r')
     valores =[]
     for line in fileref:
-        vals = line.split()
-        print(vals)
+        valores.append(int(line.split()[0]))
     fileref.close()
-    return valores
+
+    return list((maximo(valores),menor(valores),promedio(valores),lineas_totales(archivo)))
 
 def main():
     UTIL.clear_screen()
-    nombre_arch= input("Archivo a abrir: ")
-    archivo = informe(nombre_arch+".txt")
-    print(archivo)
-
+    resultados = informe("ej_7.txt")
+    print(resultados)
 
 main()
